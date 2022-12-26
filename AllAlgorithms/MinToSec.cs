1b113486c0485d8f13210ee691239ea90cdd5608
oDqnger <103481200+Diman012@users.noreply.github.com>
2022-12-26 17:24:43 +0000

    Create MinToSec.cs

---
+++ AllAlgorithms/MinToSec.cs
@@ -0,0 +1,5 @@
+
+     // converts minutes into seconds
+     public static float MinToSec(float minutes) {
+          return minutes * 60; // to find minutes to second, you simply multiple minutes by 60 because a minute has 60 seconds.
+     }
