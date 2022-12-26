bd873c00a33c514c6660583fdd96a2b24f65a782
oDqnger <103481200+Diman012@users.noreply.github.com>
2022-12-26 17:02:18 +0000

    Update TruncateSentence.cs

--- AllAlgorithms/TruncateSentence.cs
+++ AllAlgorithms/TruncateSentence.cs
@@ -1,16 +1,16 @@
 // truncate s such that it contains only the first k words. (challenge taken from leetcode)
      public string TruncateSentence(string s, int k) {
-        string ans = "";
-        int count = 0;
-        for (int x = 0; x<s.Length; x++) {
-            if (s[x] != ' ') {
-                ans = ans + s[x];
-            } else {
-                count++;
-                if (count == k) {
-                    break;
+        string ans = ""; // stores an empty var called ans (string)
+        int count = 0; // stores an empty var called count (int)
+        for (int x = 0; x<s.Length; x++) { // goes through the string
+            if (s[x] != ' ') { // checks to see if there is an empty string
+                ans = ans + s[x]; // adds s[x] to ans.
+            } else { // if it hits a space, it means that it has gone through one word, so in that case we want to increment the count var
+                count++; 
+                if (count == k) { // we check to see if count == k (which is how much we need to trucate to)
+                    break; // we break the loop once it does
                 }
-                ans = ans + s[x];
+                ans = ans + s[x]; // adds a space to s[x]
             }
         }
 
