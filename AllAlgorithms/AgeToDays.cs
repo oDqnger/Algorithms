fbafb53ef7646667b4adcfe7e1c7f7c12892859b
oDqnger <103481200+Diman012@users.noreply.github.com>
2022-12-26 17:24:55 +0000

    Create AgeToDays.cs

---
+++ AllAlgorithms/AgeToDays.cs
@@ -0,0 +1,4 @@
+// converts an age to day (ignores leap years)
+     public static int AgeToDays(int years) { 
+          return years * 360; // a year is 360 days (ignore leap years) and you multiply that by years to get days
+     }
