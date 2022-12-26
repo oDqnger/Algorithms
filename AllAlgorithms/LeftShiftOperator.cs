ef37069622127d78a4ee276587b78902dda9f379
oDqnger <103481200+Diman012@users.noreply.github.com>
2022-12-26 17:24:12 +0000

    Create LeftShiftOperator.cs

---
+++ AllAlgorithms/LeftShiftOperator.cs
@@ -0,0 +1,5 @@
+// forumla: num1 * 2^num2.
+     // left shift operator is <<
+     public static float LeftShiftByPow2(float num1, float num2) {
+          return (int)num1 * MathF.Pow(2f, num2);
+     }
